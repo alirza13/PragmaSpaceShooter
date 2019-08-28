74e1d6466e727ab691615e2fa2af76eaf08cf206
Enes Güven <enes@pragmasoft.com.tr>
2019-08-28 13:45:22 +0300

    blackhole

---
+++ PragmaSpaceShooter/Assets/Scripts/BlackHoleScripts/BlackHoleMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleMovement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float boundY = -6f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 temp = transform.position;
+        temp.y -= moveSpeed * Time.deltaTime;
+        transform.position = temp;
+
+        if (temp.y <= boundY)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ PragmaSpaceShooter/Assets/Scripts/BlackHoleScripts/BlackHoleSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleSpawner : MonoBehaviour
+{
+    public GameObject blackHolePrefab;
+
+    public float spawnTimer = 1.8f;
+    private float currentSpawnTimer = 1.8f;
+
+    public float minX = -2.5f;
+    public float maxX = 2.5f;
+
+    public float minScale = .25f;
+    public float maxScale = 1f;
+
+    void Start()
+    {
+        currentSpawnTimer = spawnTimer;
+    }
+
+    void Update()
+    {
+        SpawnObject();
+    }
+
+    void SpawnObject()
+    {
+        currentSpawnTimer += Time.deltaTime;
+
+
+        Vector3 temp = transform.position;
+        temp.x = Random.Range(minX, maxX);
+        GameObject newBlackHole = null;
+        if (currentSpawnTimer >= spawnTimer)
+        {
+            newBlackHole = Instantiate(blackHolePrefab, temp, Quaternion.identity);
+        }
+
+        if (newBlackHole)
+        {
+            float randomScale = Random.Range(minScale, maxScale);
+            newBlackHole.transform.parent = transform;
+            newBlackHole.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+            
+            currentSpawnTimer = 0f;
+        }
+
+    }
+}
